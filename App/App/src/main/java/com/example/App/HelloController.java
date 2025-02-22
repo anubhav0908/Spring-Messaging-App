b2b2c870dcc18e0f1cce48450c6f0231fd0f31a4
anubhav0908 <singhanubhav8100@gmail.com>
2025-02-22 15:21:43 +0530

    [Anubhav] add. Make REST Call to show Hello Mark Taylor from BridgeLabz Use POST Request Method and pass first name and last name in the Body

---
+++ App/App/src/main/java/com/example/App/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.App;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/UC1")
+    public String Hello() {
+        return "Hello From BridgeLabz";
+    }
+
+    @GetMapping("/UC2")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    @GetMapping("/UC3/{name}")
+    public String sayHelloWithPath(@PathVariable String name) {
+        return  "Hello " + name + " from BridgeLabz";
+    }
+
+    @PostMapping("/UC4")
+    public  String showHelloNameWithPost(@RequestBody UserDTO user){
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+
+
+}
